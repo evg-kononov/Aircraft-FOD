cb728fb7c9c6b8de68778d0f213774e16103e110
evg-kononov <cononow.jenya2015@gmail.com>
2023-10-27 11:42:04 +0500

    add wandb config

---
+++ detection/classification/config.py
@@ -0,0 +1,10 @@
+wandb_config = {
+    "data": "Aircraft-FOD.Detection.BinaryClassification",
+    "loss": "BCE"
+}
+wandb_init = {
+    "project": "Aircraft-FOD.Detection.BinaryClassification",
+    "entity": "pnrpu-ai",
+    "resume": "allow",
+    "config": wandb_config
+}
